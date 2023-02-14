41892a3987163c2c96679b6002bcf59271fead7a
L33Z22L11 <L33Z22L11@gmail.com>
2023-02-15 00:17:24 +0800

    优化计时器天数计算逻辑

--- asset/timer.js
+++ asset/timer.js
@@ -13,10 +13,13 @@ timer.find = function () {
 }
 
 timer.try = function tryingMethod(nextTitle, nextStart, nextEnd) {
-  nextEnd = new Date(nextEnd);
+  if (nextStart.length == 10) nextStart += "T00:00";
+  if (nextEnd.length == 10) nextEnd += "T00:00";
+  nextStart = new Date(`${nextStart}+08:00`);
+  nextEnd = new Date(`${nextEnd}+08:00`);
   if (timer.now > timer.end && timer.now <= nextEnd) {
     timer.title = nextTitle;
-    timer.start = new Date(nextStart);
+    timer.start = nextStart;
     timer.end = nextEnd;
   }
 };
@@ -27,11 +30,12 @@ timer.update = function () {
   let today = {
     week: Math.ceil(this.start.getDay() / 7 + (this.now - this.start) / 6048E5),
     passed: Math.ceil((this.now - this.start) / 864E5),
+    left: Math.ceil((this.end - this.now) / 864E5),
     all: Math.ceil((this.end - this.start) / 864E5),
     progress: ((this.now - this.start) / (this.end - this.start)) * 100,
   };
   this.ele[0].innerHTML = `${this.now.getMonth() + 1}.${this.now.getDate()} ${this.title}第${today.week}周`;
-  this.ele[1].innerHTML = `${today.passed}/${today.all}天 ${today.progress.toFixed(0)}%`;
+  this.ele[1].innerHTML = `第${today.passed}/${today.all}天 ${today.progress.toFixed(0)}%`;
   this.ele[2].style.width = `${today.progress}%`;
 }
 
