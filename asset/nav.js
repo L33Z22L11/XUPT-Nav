5a6c54ad2ba258597d2e99757cc798315a3e2d32
L33Z22L11 <L33Z22L11@gmail.com>
2023-03-01 22:56:04 +0800

    增加链接

--- asset/nav.js
+++ asset/nav.js
@@ -29,6 +29,8 @@ nav.list[0] = [{
   name: "考试学习", icon: "fa-solid fa-pen-ruler", item: [
     { text: "西邮试卷",desc:"欢迎commit", icon: "fa-brands fa-github", link: "https://github.com/JiaHuann/XUPT-Exam-Collection" },
     { text: "打字背单词", desc: "Qwerty Learner", link: "https://qwerty.kaiyi.cool/" },
+    { text: "洛谷", link: "https://www.luogu.com.cn/" },
+    { text: "力扣", link: "https://leetcode.cn/" },
     { text: "学信网", link: "https://www.chsi.com.cn/" },
     { text: "四六级", desc: "考试报名", link: "https://cet-bm.neea.edu.cn/" },
   ]
